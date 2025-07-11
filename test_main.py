695db2e950cd8e1b1b4fc7ec1b6d63db8dc5958e
Pfeiffer Makau <makaupfeiffer@gmail.com>
2025-07-11 12:03:30 +0300

    fix a bug

--- test_main.py
+++ test_main.py
@@ -1,8 +1,19 @@
+import pytest
 import main
 
-def test_add():
-    assert main.add(2, 3) == 5
-    assert main.add(-1, 1) == 0
-    assert main.add(0, 0) == 0
-    assert main.add(-5, -5) == -10
-    assert main.add(100, 200) == 300
+@pytest.mark.parametrize("input_values,expected_a,expected_b", [
+    (["3", "4"], 3, 4),
+    (["0", "0"], 0, 0),
+    (["-5", "10"], -5, 10),
+    (["100", "200"], 100, 200),
+])
+def test_user_input(monkeypatch, input_values, expected_a, expected_b):
+    # Create an iterator from the input values
+    input_iterator = iter(input_values)
+    # Patch input() to return values from our iterator
+    monkeypatch.setattr('builtins.input', lambda _: next(input_iterator))
+    # Call the function
+    a, b = main.user_input()
+    # Assert the results
+    assert a == expected_a
+    assert b == expected_b
