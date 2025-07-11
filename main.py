8d0cdde0410c3be7c14a951a1b856ca98bee1644
Pfeiffer Makau <makaupfeiffer@gmail.com>
2025-07-11 11:24:04 +0300

    Fixed a bug causing test automation on github actions to fail

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 def add(a, b):
     return a + b
+
+def user_input():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    return a, b
+
+def main():
+    a,b= user_input()
+    result = add(a, b)
+    print(f"The sum of {a} and {b} is {result}")
+
+if __name__ == "__main__":
+    main()
